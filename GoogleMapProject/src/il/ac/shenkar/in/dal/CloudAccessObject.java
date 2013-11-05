f305778ce63f1d22bfbf11332cba3f3ed666ae8b
cadan <cadan85@gmail.com>
2013-11-05 16:49:22 +0200

    add new icon

--- GoogleMapProject/src/il/ac/shenkar/in/dal/CloudAccessObject.java
+++ GoogleMapProject/src/il/ac/shenkar/in/dal/CloudAccessObject.java
@@ -783,6 +783,10 @@ public class CloudAccessObject implements IDataAccesObject
     {
 	// first load
 	ParseQuery<ParseObject> query = ParseQuery.getQuery("CampusInUser");
+	//dont return me, and my friends to school since by default they are my friend and i cannot remove them
+    query.whereNotEqualTo("parseUserId", curentCampusInUser.getParseUserId());
+    query.whereNotEqualTo("year", curentCampusInUser.getYear());
+    query.whereNotEqualTo("trend", curentCampusInUser.getTrend());
 	if (allUsersLastUpdate != null)
 	{
 	    query.whereGreaterThanOrEqualTo("createdAt", allUsersLastUpdate);
